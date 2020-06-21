30f9537aa80cd6dee96680ba01fa82ee1c28f263
Brunno Benatti <brunno.benatti@runsmart.cloud>
2020-06-21 17:15:58 -0300

    basic rules

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,27 @@
-import React from 'react';
-import ChessBoard from 'chessboardjsx';
-export default () => <ChessBoard position='start'/>
+import React, { useState } from 'react'
+import Chess from 'chess.js'
+import ChessBoard from 'chessboardjsx'
+const chess = new Chess()
+
+const get_board = (props) => {
+	const [state,update] = useState(props)
+	const onDrop = ({ sourceSquare,targetSquare})=>{
+		const moves = chess.moves()
+		const move = chess.move({
+			from: sourceSquare,
+			to: targetSquare,
+			promotion: 'q'
+		})
+
+		if(move===null)
+			return
+
+		chess.move(move)
+		update({ position: chess.fen() })
+	}	
+
+	state.onDrop = onDrop
+	return <ChessBoard {...state}/>
+}
+
+export default () => get_board({ position: 'start' })
