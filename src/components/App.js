e9b83e12f7dc05185692d9a7e1039835dad67067
Brunno Benatti <brunno.benatti@runsmart.cloud>
2020-06-22 00:06:10 -0300

    estilos e teste stockfish

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react'
 import Chess from 'chess.js'
 import ChessBoard from 'chessboardjsx'
-const chess = new Chess()
 
-const get_board = (props) => {
+const chess = new Chess()
+const props = { 
+	position: 'start',
+	pieceTheme: 'chess24_theme',
+	lightSquareStyle: { backgroundColor: 'rgb(250, 200, 200)' },
+	darkSquareStyle: { backgroundColor: 'rgba(15, 50, 50,.5)' },
+	boardStyle:{
+        borderRadius: "5px",
+        boxShadow: '0 5px 15px rgba(30, 0, 0, 0.5)',
+        display:'block',
+        margin:'0 auto'
+  	},
+  	dropSquareStyle:{
+		boxShadow: 'inset 0 0 1px 4px #d88'
+  	},
+}
+
+// var stockfish = new Worker("./stockfish.js");
+
+const board = (props) => {
 	const [state,update] = useState(props)
 	const onDrop = ({ sourceSquare,targetSquare})=>{
 		const moves = chess.moves()
@@ -17,11 +35,14 @@ const get_board = (props) => {
 			return
 
 		chess.move(move)
-		update({ position: chess.fen() })
+		const props = Object.assign({},state)
+		props.position = chess.fen()
+		update(props)
 	}	
 
 	state.onDrop = onDrop
-	return <ChessBoard {...state}/>
+	state.onDrop = onDrop
+	return <ChessBoard {...state} />
 }
 
-export default () => get_board({ position: 'start' })
+export default () => board(props)
